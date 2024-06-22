289e893188bb2c9aedbf7573d8cf84168cf3873a
ade-teca <adelitoteca@gmail.com>
2024-06-22 16:47:32 -0300

    atualização

---
+++ Programação orientada a objetos com java/Desafio de projeto/banco_digital/src/Cliente.java
@@ -0,0 +1,56 @@
+public class Cliente {
+
+	private String nome;
+	private String senha;
+
+	public Cliente(String nome, String senha) {
+		this.nome = nome;
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nome=" + nome + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+}
