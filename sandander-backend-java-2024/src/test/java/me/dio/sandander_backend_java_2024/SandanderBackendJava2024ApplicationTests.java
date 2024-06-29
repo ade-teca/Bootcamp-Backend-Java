8f142929e7dbe7684a7b2de2516d6b179977d7f6
ade-teca <adelitoteca@gmail.com>
2024-06-29 16:27:11 -0300

    first commit
    Java RESTful API criada

---
+++ sandander-backend-java-2024/src/test/java/me/dio/sandander_backend_java_2024/SandanderBackendJava2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.sandander_backend_java_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandanderBackendJava2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
